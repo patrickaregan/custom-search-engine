5c0f9fd14f1a2cc4f8d0371c6864f5c04e51423b
patrickaregan <patrickaregan@gmail.com>
2022-02-05 00:30:15 -0600

    add script and handler

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// https://patrickaregan.github.io/custom-search-engine/
+// Search engine id: 84bf6c59ec9dc5496
+// Public URL: https://cse.google.com/cse?cx=84bf6c59ec9dc5496
+
+// https://www.googleapis.com/customsearch/v1?[parameters]
+// https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+
+
+function handler(response) {
+    for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        // in production code, item.htmlTitle should have the HTML entities escaped.
+        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
+    }
+}
